59273d54b3b2bf66f1b52962fe649779734ebbf2
Jezukaz <jesus.sp91@gmail.com>
2017-01-18 13:57:05 -0500

    Modificacion de index.php

--- index.php
+++ index.php
@@ -8,17 +8,14 @@
 
         <div class="page-inner">
             <div class="page-title">
-                <h3>Form Elements</h3>
+                <h3>Formulario de Registro</h3>
             </div>
             <div id="main-wrapper">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="panel panel-white">
-                            <div class="panel-heading clearfix">
-                                <h4 class="panel-title">Form Elements</h4>
-                            </div>
                             <div class="panel-body">
-                                <form class="form-horizontal" action="registro.php" method="POST">
+                                <div role="form" onkeypress="return runScriptReg(event)">
                                     <input type="hidden" name="fu" value="nuevo">
                                     <div class="form-group">
                                         <label for="input-Default" class="col-sm-2 control-label">Nombre</label>
@@ -45,40 +42,26 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="input-Default" class="col-sm-2 control-label">Genero</label>
-                                        <div class="col-sm-6">
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="sexo" value="1" checked="">
-                                            <input type="text" class="form-control" id="apm" name="apm">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="input-Default" class="col-sm-2 control-label">DNI</label>
-                                        <div class="col-sm-2">
-                                            <input type="text" class="form-control" id="dni" name="dni">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
                                         <label for="input-Default" class="col-sm-2 control-label">Sexo</label>
-                                        <div class="col-sm-6">
+                                        <div class="col-sm-6" id="rates">
                                             <div class="radio">
                                                 <label>
-                                                    <input type="radio" name="ms" value="1" checked="">
+                                                    <input type="radio" id="ms" name="rate" value="1" checked="">
                                                    Masculino
                                                 </label>
                                             </div>
                                             <div class="radio">
                                                 <label>
-                                                    <input type="radio" name="fm" value="0">
+                                                    <input type="radio" id="fm" name="rate" value="0">
                                                     Femenino
                                                 </label>
                                             </div>                                                                    
                                         </div>
                                     </div>   
+
                                     <div class="form-group">
-                                        <label class="col-sm-3 control-label">Fecha Nacimiento</label>
-                                        <div class="col-sm-6">
+                                        <label for="input-Default" class="col-sm-2 control-label">Fecha Nacimiento</label>
+                                        <div class="col-sm-2">
                                             <div class="input-group">
                                                 <div class="input-group-addon">
                                                     <i class="fa fa-calendar"></i>
@@ -89,30 +72,21 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-sm-3 control-label">Telefono</label>
-                                        <div class="col-sm-6">
+                                        <label for="input-Default" class="col-sm-2 control-label">Telefono</label>
+                                        <div class="col-sm-2">
                                             <input type="text" class="form-control" id="telefono" name="telefono">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-sm-3 control-label">Direccion</label>
-                                        <div class="col-sm-6">
-                                            <input type="text" class="form-control" id="inputError" name="direccion">
-
-                                        <div class="col-sm-6">
-                                            <input type="text" class="form-control" id="telefono" name="telefono">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-3 control-label">Direccion</label>
-                                        <div class="col-sm-6">
+                                        <label for="input-Default" class="col-sm-2 control-label">Direccion</label>
+                                        <div class="col-sm-2">
                                             <input type="text" class="form-control" id="direccion" name="direccion">
 
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-sm-3 control-label">Correo Electronico</label>
-                                        <div class="col-sm-6">
+                                        <label for="input-Default" class="col-sm-2 control-label">Correo Electronico</label>
+                                        <div class="col-sm-2">
                                             <input type="text" class="form-control" id="email" name="email">
 
                                         </div>
@@ -125,10 +99,10 @@
                                     </div>
                                     <div class="col-sm-6">
                                         <button type="reset" class="btn btn-default">Cancelar</button>
-                                        <button type="submit" class="btn btn-info pull-right">Registrar</button>
+                                        <button type="button" onclick="goReg()" class="btn btn-info pull-right">Registrar</button>
                                     </div>
                                     
-                                </form>
+                                </div>
                                
                             </div>
                         </div>
